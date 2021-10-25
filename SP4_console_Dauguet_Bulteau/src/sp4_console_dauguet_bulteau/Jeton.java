4d7fc74bdecd69aed73452e4fab38b8bdbf4e929
bulte <bulte@LAPTOP-9OIFTFDN>
2021-10-25 12:59:04 +0200

    Le TP du puissance 4 complique

---
+++ SP4_console_Dauguet_Bulteau/src/sp4_console_dauguet_bulteau/Jeton.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_dauguet_bulteau;
+
+/**
+ *
+ * @author bulte
+ */
+public class Jeton {
+    String couleur;
+    public void jeton(String unecouleur){
+        couleur=unecouleur;
+    }
+    public String lireCouleur(){
+        return(couleur);
+    }
+    
+}
