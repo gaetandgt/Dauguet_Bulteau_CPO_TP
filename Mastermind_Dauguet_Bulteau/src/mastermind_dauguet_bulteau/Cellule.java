d3bbe823bad522068fe039604fde114395e6a5fc
gaeta <gaeta@LAPTOP-UHS24O91>
2021-12-03 12:27:16 +0100

    modif

---
+++ Mastermind_Dauguet_Bulteau/src/mastermind_dauguet_bulteau/Cellule.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mastermind_dauguet_bulteau;
+
+/**
+ *
+ * @author gaeta
+ */
+public class Cellule {
+    String Couleur;
+    
+public Cellule(String unecouleur){
+    Couleur=unecouleur;
+    }
+
+public String lireCouleur(){
+    return(Couleur);
+}
+
+}
